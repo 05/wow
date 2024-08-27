9920475712eec9a82fdeedf1efaf2f24e8ac86ca
05 <110515403+05@users.noreply.github.com>
2024-08-27 19:51:21 -0400

    d configurations

--- wc/wc/wc/wc.cpp
+++ wc/wc/wc/wc.cpp
@@ -9,48 +9,49 @@
 #include <chrono>
 #include <shellapi.h>
 #include <functional>
+
 #define IDD_DIALOG_CPS 101
 #define IDC_EDIT_CPS 1001
 #define IDI_TRAYICON 102  // Add this line
 
 #pragma comment(lib, "winmm.lib")
 
-namespace vars {
-    int cps{ 0 };
+namespace {
+    int cps = 0;
     HWND minecraft_window{};
-    std::atomic<bool> running{ true };
+    std::atomic<bool> running{true};
     std::vector<int> delays;
     NOTIFYICONDATA nid = {};
-    HMENU hMenu = nullptr;
+    HMENU menu = nullptr;
     std::vector<int> right_click_delays;
     std::atomic<bool> paused{ false };
 }
 
 namespace math {
-    int get_random_int(int min, int max) {
-        static thread_local std::mt19937 generator(std::random_device{}());
+    int get_random_int(const int min, const int max) {
+	    thread_local std::mt19937 generator(std::random_device{}());
         std::uniform_int_distribution<int> distribution(min, max);
         return distribution(generator);
     }
 }
 
 namespace nt {
-    typedef NTSTATUS(NTAPI* NtDelayExecutionFunc)(BOOLEAN Alertable, PLARGE_INTEGER DelayInterval);
-    NtDelayExecutionFunc NtDelayExecution = nullptr;
+    typedef NTSTATUS(NTAPI* NtDelayExecutionFunc)(BOOLEAN alert, PLARGE_INTEGER delay_interval);
+    NtDelayExecutionFunc nt_delay_execution = nullptr;
 
-    void Sleep(LONGLONG delay) {
-        if (!NtDelayExecution) {
-            HMODULE ntdll = GetModuleHandleA("ntdll.dll");
-            if (ntdll) {
-                NtDelayExecution = reinterpret_cast<NtDelayExecutionFunc>(
+    void sleep(const LONGLONG delay) {
+        if (!nt_delay_execution) {
+	        const HMODULE ntdll = GetModuleHandleA("ntdll.dll");
+
+            if (ntdll)
+                nt_delay_execution = reinterpret_cast<NtDelayExecutionFunc>(
                     GetProcAddress(ntdll, "NtDelayExecution"));
-            }
         }
 
-        if (NtDelayExecution) {
+        if (nt_delay_execution) {
             LARGE_INTEGER li;
             li.QuadPart = -delay * 10000LL;
-            NtDelayExecution(FALSE, &li);
+            nt_delay_execution(FALSE, &li);
         }
         else {
             ::Sleep(static_cast<DWORD>(delay));
@@ -58,89 +59,89 @@ namespace nt {
     }
 }
 
-void generate_delays(int cps) {
-    vars::delays.clear();
-    double base_delay = 1000.0 / cps;
-    double variation_range = base_delay * 0.1;
+void generate_delays(const int cps) {
+    delays.clear();
+    const double base_delay = 1000.0 / cps;
+    const double variation_range = base_delay * 0.1;
 
     for (int x = 0; x < 5000; x++) {
-        double variation = math::get_random_int(-100, 100) * variation_range / 100.0;
+	    const double variation = math::get_random_int(-100, 100) * variation_range / 100.0;
         int delay = (std::max)(1, static_cast<int>(std::round(base_delay + variation)));
-        vars::delays.push_back(delay);
+        delays.push_back(delay);
     }
 }
 
 void rotate_delays() {
-    std::rotate(vars::delays.begin(), vars::delays.begin() + 1, vars::delays.end());
+    std::rotate(delays.begin(), delays.begin() + 1, delays.end());
 }
 
-void LeftMouseClick() {
-    PostMessageA(vars::minecraft_window, WM_LBUTTONDOWN, MK_LBUTTON, 0);
-    PostMessageA(vars::minecraft_window, WM_LBUTTONUP, MK_LBUTTON, 0);
-    nt::Sleep(static_cast<LONGLONG>(vars::delays.front()));
+void left_mouse_click() {
+    PostMessageA(minecraft_window, WM_LBUTTONDOWN, MK_LBUTTON, 0);
+    PostMessageA(minecraft_window, WM_LBUTTONUP, MK_LBUTTON, 0);
+    nt::sleep(delays.front());
     rotate_delays();
 }
 
 void generate_right_click_delays(int cps) {
-    vars::right_click_delays.clear();
-    double base_delay = 1000.0 / (cps * 0.7);  // 70% of the left-click speed (30% slower)
-    double variation_range = base_delay * 0.1;
+    right_click_delays.clear();
+    const double base_delay = 1000.0 / (cps * 0.7);  // 70% of the left-click speed (30% slower)
+    const double variation_range = base_delay * 0.1;
 
     for (int x = 0; x < 5000; x++) {
-        double variation = math::get_random_int(-100, 100) * variation_range / 100.0;
+	    const double variation = math::get_random_int(-100, 100) * variation_range / 100.0;
         int delay = (std::max)(1, static_cast<int>(std::round(base_delay + variation)));
-        vars::right_click_delays.push_back(delay);
+        right_click_delays.push_back(delay);
     }
 }
 
 void rotate_right_click_delays() {
-    std::rotate(vars::right_click_delays.begin(), vars::right_click_delays.begin() + 1, vars::right_click_delays.end());
+    std::rotate(right_click_delays.begin(), right_click_delays.begin() + 1, right_click_delays.end());
 }
 
-void RightMouseClick() {
-    PostMessageA(vars::minecraft_window, WM_RBUTTONDOWN, MK_RBUTTON, 0);
-    PostMessageA(vars::minecraft_window, WM_RBUTTONUP, MK_RBUTTON, 0);
-    nt::Sleep(static_cast<LONGLONG>(vars::right_click_delays.front()));
+void right_mouse_click() {
+    PostMessageA(minecraft_window, WM_RBUTTONDOWN, MK_RBUTTON, 0);
+    PostMessageA(minecraft_window, WM_RBUTTONUP, MK_RBUTTON, 0);
+    nt::sleep(static_cast<LONGLONG>(right_click_delays.front()));
     rotate_right_click_delays();
 }
 
-void Mouse5Click() {
+void mouse5_click() {
     keybd_event('2', 0, 0, 0);
-    nt::Sleep(15);
+    nt::sleep(15);
     keybd_event('2', 0, KEYEVENTF_KEYUP, 0);
-    nt::Sleep(7);
-    PostMessageA(vars::minecraft_window, WM_RBUTTONDOWN, MK_RBUTTON, 0);
-    nt::Sleep(7);
-    PostMessageA(vars::minecraft_window, WM_RBUTTONUP, MK_RBUTTON, 0);
-    nt::Sleep(15);
+    nt::sleep(7);
+    PostMessageA(minecraft_window, WM_RBUTTONDOWN, MK_RBUTTON, 0);
+    nt::sleep(7);
+    PostMessageA(minecraft_window, WM_RBUTTONUP, MK_RBUTTON, 0);
+    nt::sleep(15);
     keybd_event('1', 0, 0, 0);
-    nt::Sleep(15);
+    nt::sleep(15);
     keybd_event('1', 0, KEYEVENTF_KEYUP, 0);
-    nt::Sleep(15);
+    nt::sleep(15);
 }
 
 void RemoveSystemTrayIcon() {
-    Shell_NotifyIcon(NIM_DELETE, &vars::nid);
+    Shell_NotifyIcon(NIM_DELETE, &nid);
 }
 
 void CreateSystemTrayIcon(HWND hwnd) {
-    vars::nid.cbSize = sizeof(NOTIFYICONDATA);
-    vars::nid.hWnd = hwnd;
-    vars::nid.uID = 1;
-    vars::nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
-    vars::nid.uCallbackMessage = WM_USER + 1;
+    nid.cbSize = sizeof(NOTIFYICONDATA);
+    nid.hWnd = hwnd;
+    nid.uID = 1;
+    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+    nid.uCallbackMessage = WM_USER + 1;
     
     // Load the custom icon
     HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_TRAYICON));
     if (hIcon) {
-        vars::nid.hIcon = hIcon;
+        nid.hIcon = hIcon;
     } else {
         // Fallback to default application icon if custom icon fails to load
-        vars::nid.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+        nid.hIcon = LoadIcon(NULL, IDI_APPLICATION);
     }
     
-    wcscpy_s(vars::nid.szTip, L"WC Autoclicker");
-    Shell_NotifyIcon(NIM_ADD, &vars::nid);
+    wcscpy_s(nid.szTip, L"WC Autoclicker");
+    Shell_NotifyIcon(NIM_ADD, &nid);
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
@@ -151,35 +152,48 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             POINT pt;
             GetCursorPos(&pt);
             SetForegroundWindow(hwnd);
-            UINT clicked = TrackPopupMenu(vars::hMenu, TPM_RETURNCMD | TPM_NONOTIFY, pt.x, pt.y, 0, hwnd, NULL);
+
+            const UINT clicked = TrackPopupMenu(
+                menu, 
+                TPM_RETURNCMD | TPM_NONOTIFY, 
+                pt.x, pt.y, 
+                0, 
+                hwnd,
+                nullptr
+            );
+
             if (clicked == 1) {
-                vars::running = false;
+                running = false;
                 PostQuitMessage(0);
             }
         }
         break;
+
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
+
+	default: break;
     }
+
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
-INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+INT_PTR CALLBACK dialog_proc(const HWND hwnd_dlg, const UINT u_msg, const WPARAM w_param, LPARAM l_param)
 {
-    static HBRUSH hBrushBackground = CreateSolidBrush(RGB(0, 0, 0));
+    static HBRUSH h_brush_background = CreateSolidBrush(RGB(0, 0, 0));
 
-    switch (uMsg)
+    switch (u_msg)
     {
     case WM_INITDIALOG:
         {
             // Set the background color to black for the dialog
-            SetClassLongPtr(hwndDlg, GCLP_HBRBACKGROUND, (LONG_PTR)hBrushBackground);
+            SetClassLongPtr(hwnd_dlg, GCLP_HBRBACKGROUND, reinterpret_cast<LONG_PTR>(h_brush_background));
             
             // Set the text color to white for all child controls
-            EnumChildWindows(hwndDlg, [](HWND hwnd, LPARAM lParam) -> BOOL {
+            EnumChildWindows(hwnd_dlg, [](HWND hwnd, LPARAM l_param) -> BOOL {
                 SetWindowLongPtr(hwnd, GWL_STYLE, GetWindowLongPtr(hwnd, GWL_STYLE) | WS_CHILD);
-                return TRUE;
+                return true;
             }, 0);
 
             return TRUE;
@@ -190,29 +204,29 @@ INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPara
     case WM_CTLCOLOREDIT:
     case WM_CTLCOLORBTN:
         {
-            HDC hdcStatic = (HDC)wParam;
-            SetTextColor(hdcStatic, RGB(255, 255, 255));  // white text
-            SetBkColor(hdcStatic, RGB(0, 0, 0));  // gray background
-            return (INT_PTR)hBrushBackground;
+	        const auto hdc_static = reinterpret_cast<HDC>(w_param);
+            SetTextColor(hdc_static, RGB(255, 255, 255));  // white text
+            SetBkColor(hdc_static, RGB(0, 0, 0));  // gray background
+            return reinterpret_cast<INT_PTR>(h_brush_background);
         }
 
     case WM_COMMAND:
-        switch (LOWORD(wParam))
+        switch (LOWORD(w_param))
         {
         case IDOK:
             // Get the CPS value from the edit control
-            vars::cps = GetDlgItemInt(hwndDlg, IDC_EDIT_CPS, NULL, FALSE);
-            EndDialog(hwndDlg, IDOK);
+            cps = GetDlgItemInt(hwnd_dlg, IDC_EDIT_CPS, NULL, FALSE);
+            EndDialog(hwnd_dlg, IDOK);
             return TRUE;
 
         case IDCANCEL:
-            EndDialog(hwndDlg, IDCANCEL);
+            EndDialog(hwnd_dlg, IDCANCEL);
             return TRUE;
         }
         break;
 
     case WM_DESTROY:
-        DeleteObject(hBrushBackground);
+        DeleteObject(h_brush_background);
         return TRUE;
     }
     return FALSE;
@@ -223,27 +237,27 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     std::cout << "--------------------\n" << "CPS CONVERSION CHART\n";
     std::cout << "   20 = 12-14 cps\n   30 = 17-19 cps\n   40 = 21-23 cps\n   50 = 22-24 cps\n" << "--------------------\n\n";
 
-    INT_PTR result = DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG_CPS), NULL, DialogProc);
+    const INT_PTR result = DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG_CPS), NULL, dialog_proc);
     if (result == -1) {
-        DWORD error = GetLastError();
+	    const DWORD error = GetLastError();
         char errorMsg[256];
         sprintf_s(errorMsg, "Failed to create dialog. Error code: %d", error);
-        MessageBoxA(NULL, errorMsg, "Error", MB_ICONERROR);
+        MessageBoxA(nullptr, errorMsg, "Error", MB_ICONERROR);
         return -1;
     }
-    else if (result != IDOK) {
-        MessageBoxA(NULL, "Dialog cancelled. Exiting application.", "Information", MB_ICONINFORMATION);
-        return 0;
+    if (result != IDOK) {
+	    MessageBoxA(nullptr, "Dialog cancelled. Exiting application.", "Information", MB_ICONINFORMATION);
+	    return 0;
     }
 
-    vars::minecraft_window = FindWindowA("LWJGL", nullptr);
-    if (!vars::minecraft_window) {
+    minecraft_window = FindWindowA("LWJGL", nullptr);
+    if (!minecraft_window) {
         std::cerr << "minecraft window not found!\n";
         return -1;
     }
     
-    generate_delays(vars::cps);
-    Mouse5Click();
+    generate_delays(cps);
+    mouse5_click();
 
     std::cout << "autoclicker started. press 'F1' to exit.\n";
 
@@ -267,44 +281,44 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     CreateSystemTrayIcon(hwnd);
     
     // Create system tray menu
-    vars::hMenu = CreatePopupMenu();
-    AppendMenu(vars::hMenu, MF_STRING, 1, L"Exit");
+    menu = CreatePopupMenu();
+    AppendMenu(menu, MF_STRING, 1, L"Exit");
 
     MSG msg;
-    while (vars::running) {
+    while (running) {
         while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
 
-        auto currentTime = std::chrono::steady_clock::now();
+        auto current_time = std::chrono::steady_clock::now();
 
         // Toggle pause with Mouse4
         if ((GetAsyncKeyState(VK_XBUTTON1) & 0x8000) &&
-            (currentTime - lastToggleClick > toggleCooldown)) {
-            vars::paused = !vars::paused;
-            lastToggleClick = currentTime;
+            (current_time - lastToggleClick > toggleCooldown)) {
+            paused = !paused;
+            lastToggleClick = current_time;
             // Optional: Add some feedback (e.g., console output or system tray notification)
-            std::cout << (vars::paused ? "Autoclicker paused" : "Autoclicker resumed") << std::endl;
+            std::cout << (paused ? "Autoclicker paused" : "Autoclicker resumed") << std::endl;
         }
 
-        if (!vars::paused) {
+        if (!paused) {
             if (GetAsyncKeyState(VK_LBUTTON) & 0x8000) {
-                LeftMouseClick();
+                left_mouse_click();
             }
 
             if ((GetAsyncKeyState(VK_XBUTTON2) & 0x8000) &&
-                (currentTime - lastMouse5Click > mouse5Cooldown)) {
-                Mouse5Click();
-                lastMouse5Click = currentTime;
+                (current_time - lastMouse5Click > mouse5Cooldown)) {
+                mouse5_click();
+                lastMouse5Click = current_time;
             }
         }
 
         if (GetAsyncKeyState(VK_F1) & 0x8000) {
-            vars::running = false;
+            running = false;
         }
 
-        nt::Sleep(1);
+        nt::sleep(1);
     }
     RemoveSystemTrayIcon();
     return 0;
